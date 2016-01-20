6e5942c752a45b93a46ceb4c404a2c5e83ebe3a5
Snake.Leon <snakeleon@sina.com>
2016-01-20 21:05:41 +0800

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,19 +1,24 @@
-#include <stdio.h>
 #include <iostream>
 
-class a
-{
-    public:
-        int b(int);
-};
 
-int main()
+typedef struct X
 {
-    printf("Helo World!\n");
-    std::cout<<"Hello World!"<<std::endl;
-    a b;
-    b.b(0);
-    a *p;
-    p->b(1);
+    int a;
+    int b;
+    int c;
+    int d;
+}X;
+
+
+int main(void)
+{
+    X A = { 1, 2, 3, 4 };
+    X B = { 5, 6, 7, 8 };
+    X C = { 4, 5, 6, 7 };
+    std::cout<<A.a <<A.b <<A.c <<A.d <<std::endl;
+    std::cout<<B.a <<B.b <<B.c <<B.d <<std::endl;
+    std::cout<<C.a <<C.b <<C.c <<C.d <<std::endl;
+    //getch();
+
     return 0;
 }
